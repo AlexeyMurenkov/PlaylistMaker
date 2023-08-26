b4bd0a7daffbf483a12548351005f25bb55caa74
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-08-26 19:32:29 +0300

    feat: Спринт 8/23 → Тема 5/6: Навигация

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,31 +1,28 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonOnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, (v as Button).text, Toast.LENGTH_SHORT).show()
-            }
-        }
-
         val buttonFind = findViewById<Button>(R.id.find)
-        buttonFind.setOnClickListener(buttonOnClickListener)
+        buttonFind.setOnClickListener {
+            startActivity(Intent(this, FindActivity::class.java))
+        }
 
         val buttonMedia = findViewById<Button>(R.id.media)
         buttonMedia.setOnClickListener{
-            Toast.makeText(this@MainActivity, (it as Button).text, Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MediaActivity::class.java))
         }
 
         val buttonSettings = findViewById<Button>(R.id.settings)
-        buttonSettings.setOnClickListener(buttonOnClickListener)
+        buttonSettings.setOnClickListener{
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
