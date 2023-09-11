3f80a9bfb2a08babfa15284c8c0383b1634c573b
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-09-11 22:09:42 +0300

    feat: Спринт 9/23 → Тема 2/5: Context и Intent

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -10,9 +12,48 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val textBack = findViewById<TextView>(R.id.settings_back)
-
         textBack.setOnClickListener {
             finish()
         }
+
+        val shareApp = findViewById<TextView>(R.id.settings_share_app)
+        shareApp.setOnClickListener {
+            shareApp()
+        }
+
+        val support = findViewById<TextView>(R.id.settings_support)
+        support.setOnClickListener {
+            mailToSupport()
+        }
+
+        val agreement = findViewById<TextView>(R.id.settings_agreement)
+        agreement.setOnClickListener {
+            showAgreement()
+        }
+    }
+
+    fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_text))
+            .type = "text/plain"
+        startActivity(intent)
+    }
+
+    fun mailToSupport() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent
+            .setData(Uri.parse("mailto:"))
+            .putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_mail)))
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_default_subject))
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.support_default_text))
+        startActivity(intent)
+    }
+
+    fun showAgreement() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent
+            .setData(Uri.parse(getString(R.string.agreement)))
+        startActivity(intent)
     }
 }
