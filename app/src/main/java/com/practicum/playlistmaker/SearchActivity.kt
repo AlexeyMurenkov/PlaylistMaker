dbccea74551eaf1efeefd958663d4e2731ff7815
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-09-19 23:40:12 +0300

    feat: Доработка для точного выполнения требований задачи спринта

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,10 +1,12 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -25,14 +27,11 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val searchEditText = findViewById<EditText>(R.id.search_text)
-        if (savedInstanceState != null) {
-            val restoredText = savedInstanceState.getString(SEARCH_TEXT)
-            searchEditText.setText(restoredText)
-        }
 
         val searchClear = findViewById<ImageView>(R.id.search_clear)
         searchClear.setOnClickListener {
             searchEditText.setText("")
+            hideKeyboard(searchEditText)
         }
 
         val searchTextWatcher = object : TextWatcher {
@@ -56,5 +55,17 @@ class SearchActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         val searchEditText = findViewById<EditText>(R.id.search_text)
         outState.putString(SEARCH_TEXT, searchEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchEditText = findViewById<EditText>(R.id.search_text)
+        val restoredText = savedInstanceState.getString(SEARCH_TEXT)
+        searchEditText.setText(restoredText)
+    }
+
+    fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
