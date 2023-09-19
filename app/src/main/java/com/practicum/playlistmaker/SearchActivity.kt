35b9a231a6d6acd7e84fa25e7131a734022a8058
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-09-19 22:27:12 +0300

    feat: Спринт 9/23 → Тема 3/5: TextWatcher

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,6 +2,11 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
 class SearchActivity : AppCompatActivity() {
@@ -13,5 +18,28 @@ class SearchActivity : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+
+        val searchText = findViewById<EditText>(R.id.search_text)
+        val searchClear = findViewById<ImageView>(R.id.search_clear)
+        searchClear.setOnClickListener {
+            searchText.setText("")
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchClear.visibility = if (s.isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+        searchText.addTextChangedListener(searchTextWatcher)
+
     }
 }
