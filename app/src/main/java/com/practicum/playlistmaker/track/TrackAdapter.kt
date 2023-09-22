99b864be3ba76ac79b34c446e6e72519e8f031e0
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-09-22 22:42:01 +0300

    feat: Исправления по замечаниям ревьювера

--- app/src/main/java/com/practicum/playlistmaker/track/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/track/TrackAdapter.kt
@@ -4,15 +4,22 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.utils.dpToPx
 
 class TrackAdapter(
     private val tracks: List<Track>
 ) : Adapter<TrackViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val imageCornersPx = parent
+            .getResources()
+            .getDimension(R.dimen.dimen_2)
+
+        val imageCornersDp = dpToPx(imageCornersPx, parent.context)
+
         val view = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.card_track, parent,false)
-        return TrackViewHolder(view)
+        return TrackViewHolder(view, imageCornersDp)
     }
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
--- app/src/main/java/com/practicum/playlistmaker/track/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/track/TrackViewHolder.kt
@@ -7,32 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.R
-import com.practicum.playlistmaker.utils.dpToPx
 
-class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class TrackViewHolder(view: View, val imageCornersDp: Int) : RecyclerView.ViewHolder(view) {
 
-    private val name: TextView
-    private val artist: TextView
-    private val time: TextView
-    private val image: ImageView
-
-    init {
-        name = view.findViewById(R.id.track_name)
-        artist = view.findViewById(R.id.track_artist)
-        time = view.findViewById(R.id.track_time)
-        image = view.findViewById(R.id.track_image)
-    }
+    private val name: TextView = view.findViewById(R.id.track_name)
+    private val artist: TextView = view.findViewById(R.id.track_artist)
+    private val time: TextView = view.findViewById(R.id.track_time)
+    private val image: ImageView = view.findViewById(R.id.track_image)
 
     fun bind(track: Track) {
         name.text = track.trackName
         artist.text = track.artistName
         time.text = track.trackTime
-
-        val imageCornersPx = itemView
-            .getResources()
-            .getDimension(R.dimen.dimen_2)
-
-        val imageCornersDp = dpToPx(imageCornersPx, itemView.context)
 
         Glide.with(itemView)
             .load(track.artworkUrl100)
