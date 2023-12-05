2a3adc43eef7524abbd7813a4acdd4cc8d6ac8bb
AlexeyMurenkov <murenkovalexey@gmail.com>
2023-12-05 16:12:38 +0300

    feat: Задача спринта №14 - Аудиоплеер

--- app/src/main/java/com/practicum/playlistmaker/activity/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/activity/PlayerActivity.kt
@@ -1,7 +1,10 @@
 package com.practicum.playlistmaker.activity
 
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -16,6 +19,13 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class PlayerActivity : AppCompatActivity() {
+
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val positionUpdater = Runnable { positionUpdate() }
+
 
     lateinit var binding: ActivityPlayerBinding
 
@@ -33,11 +43,16 @@ class PlayerActivity : AppCompatActivity() {
 
         with(binding) {
             setContentView(root)
+            playerPlay.setOnClickListener {
+                playPause()
+            }
+
             playerBack.setOnClickListener {
                 finish()
             }
         }
         bind(track!!)
+        preparePlayer(track)
     }
 
     private fun bind(track: Track) {
@@ -62,6 +77,13 @@ class PlayerActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        if (playerState == STATE_PLAYING) {
+            playPause()
+        }
+    }
+
     private fun loadTrackBigImage(track: Track) {
         Glide.with(this)
             .load(track.getArtworkUrl512())
@@ -73,5 +95,62 @@ class PlayerActivity : AppCompatActivity() {
                 )
             )
             .into(binding.playerCover)
+    }
+
+    private fun positionUpdate() {
+        if (mediaPlayer.currentPosition >= mediaPlayer.duration) {
+            mediaPlayer.pause()
+            mediaPlayer.seekTo(0)
+            playerState = STATE_PAUSED
+            setButtonState()
+        }
+        binding.playerProgress.text = formatTrackTime(mediaPlayer.currentPosition)
+        if (playerState == STATE_PLAYING) {
+            handler.postDelayed(positionUpdater, 1000L)
+        }
+    }
+    private fun setButtonState() {
+        if (playerState == STATE_PLAYING) {
+            binding.playerPlay.setImageResource(R.drawable.button_pause)
+            positionUpdate()
+        } else {
+            binding.playerPlay.setImageResource(R.drawable.button_play)
+            handler.removeCallbacks(positionUpdater)
+        }
+        binding.playerPlay.isEnabled = playerState != STATE_DEFAULT
+    }
+
+    private fun preparePlayer(track: Track) {
+        if (track.previewUrl.isNullOrBlank()) return
+        with(mediaPlayer) {
+            setDataSource(track.previewUrl)
+            setOnPreparedListener {
+                playerState = STATE_PREPARED
+                setButtonState()
+                positionUpdate()
+            }
+            prepareAsync()
+        }
+    }
+
+    private fun playPause() {
+        with(mediaPlayer) {
+            if (playerState == STATE_PLAYING) {
+                pause()
+                playerState = STATE_PAUSED
+            } else {
+                start()
+                playerState = STATE_PLAYING
+            }
+        }
+        setButtonState()
+        positionUpdate()
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
     }
 }
